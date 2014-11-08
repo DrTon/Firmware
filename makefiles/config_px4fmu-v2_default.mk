3ac7b3b00328493d743aaaa0ab82c84e0f462f56
Anton Babushkin <anton.babushkin@me.com>
2014-11-08 23:27:06 +0100

    Remove unused (for MC) modules from build

--- makefiles/config_px4fmu-v2_default.mk
+++ makefiles/config_px4fmu-v2_default.mk
@@ -26,20 +26,20 @@ MODULES		+= drivers/lsm303d
 MODULES		+= drivers/l3gd20
 MODULES		+= drivers/hmc5883
 MODULES		+= drivers/ms5611
-MODULES		+= drivers/mb12xx
-MODULES		+= drivers/sf0x
-MODULES		+= drivers/ll40ls
+#MODULES		+= drivers/mb12xx
+#MODULES		+= drivers/sf0x
+#MODULES		+= drivers/ll40ls
 MODULES		+= drivers/gps
-MODULES		+= drivers/hil
-MODULES		+= drivers/hott/hott_telemetry
-MODULES		+= drivers/hott/hott_sensors
-MODULES		+= drivers/blinkm
-MODULES		+= drivers/airspeed
-MODULES		+= drivers/ets_airspeed
-MODULES		+= drivers/meas_airspeed
+#MODULES		+= drivers/hil
+#MODULES		+= drivers/hott/hott_telemetry
+#MODULES		+= drivers/hott/hott_sensors
+#MODULES		+= drivers/blinkm
+#MODULES		+= drivers/airspeed
+#MODULES		+= drivers/ets_airspeed
+#MODULES		+= drivers/meas_airspeed
 MODULES		+= drivers/frsky_telemetry
 MODULES		+= modules/sensors
-MODULES		+= drivers/mkblctrl
+#MODULES		+= drivers/mkblctrl
 MODULES		+= drivers/pca8574
 MODULES		+= drivers/px4flow
 
@@ -61,7 +61,7 @@ MODULES		+= systemcmds/pwm
 MODULES		+= systemcmds/esc_calib
 MODULES		+= systemcmds/reboot
 MODULES		+= systemcmds/top
-MODULES		+= systemcmds/tests
+#MODULES		+= systemcmds/tests
 MODULES		+= systemcmds/config
 MODULES		+= systemcmds/nshterm
 MODULES		+= systemcmds/mtd
@@ -75,23 +75,23 @@ MODULES		+= modules/commander
 MODULES		+= modules/navigator
 MODULES		+= modules/mavlink
 MODULES		+= modules/gpio_led
-MODULES		+= modules/uavcan
+#MODULES		+= modules/uavcan
 
 #
 # Estimation modules (EKF/ SO3 / other filters)
 #
 MODULES		+= modules/attitude_estimator_ekf
-MODULES		+= modules/attitude_estimator_so3
-MODULES		+= modules/ekf_att_pos_estimator
+#MODULES		+= modules/attitude_estimator_so3
+#MODULES		+= modules/ekf_att_pos_estimator
 MODULES		+= modules/position_estimator_inav
-MODULES		+= examples/flow_position_estimator
+#MODULES		+= examples/flow_position_estimator
 
 #
 # Vehicle Control
 #
 #MODULES		+= modules/segway # XXX Needs GCC 4.7 fix
-MODULES		+= modules/fw_pos_control_l1
-MODULES		+= modules/fw_att_control
+#MODULES		+= modules/fw_pos_control_l1
+#MODULES		+= modules/fw_att_control
 MODULES		+= modules/mc_att_control
 MODULES		+= modules/mc_pos_control
 
@@ -121,17 +121,17 @@ MODULES		+= modules/dataman
 LIBRARIES	+= lib/mathlib/CMSIS
 MODULES		+= lib/mathlib
 MODULES		+= lib/mathlib/math/filter
-MODULES		+= lib/ecl
-MODULES		+= lib/external_lgpl
+#MODULES		+= lib/ecl
+#MODULES		+= lib/external_lgpl
 MODULES		+= lib/geo
 MODULES		+= lib/geo_lookup
 MODULES		+= lib/conversion
-MODULES		+= lib/launchdetection
+#MODULES		+= lib/launchdetection
 
 #
 # OBC challenge
 #
-MODULES		+= modules/bottle_drop
+#MODULES		+= modules/bottle_drop
 
 #
 # Demo apps
@@ -139,7 +139,7 @@ MODULES		+= modules/bottle_drop
 #MODULES		+= examples/math_demo
 # Tutorial code from
 # https://pixhawk.ethz.ch/px4/dev/hello_sky
-MODULES		+= examples/px4_simple_app
+#MODULES		+= examples/px4_simple_app
 
 # Tutorial code from
 # https://pixhawk.ethz.ch/px4/dev/daemon
